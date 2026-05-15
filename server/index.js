97aff2ad87a5d08df30ba05bfa7a40579ef049f5
Nathnael <dnathnael00@gmail.com>
2026-05-15 23:15:27 +0300

    Fix rate limiter message formatting

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  message: { message: 'Too many requests, please try again later' }
+  message: { message: 'Too many requests, please try again later' },
+  keyGenerator: (req) => req.headers['x-forwarded-for'] || req.ip
 });
-
 app.use(helmet());
 app.use(compression());
 app.use(limiter);
