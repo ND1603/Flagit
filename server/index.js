3ae60d2027ba65d9d637958d08dda902427f524c
Nathnael <dnathnael00@gmail.com>
2026-05-29 21:10:25 +0300

    Update rate limiter configuration
    Removed custom message and legacy headers from rate limiter configuration.

--- server/index.js
+++ server/index.js
@@ -21,10 +21,7 @@ const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
-  message: { message: 'Too many requests, please try again later' },
   keyGenerator: (req) => req.headers['x-forwarded-for'] || req.ip
-  legacyHeaders: false,
-  standardHeaders: 'draft-7'
 });
 app.use(helmet());
 app.use(compression());
