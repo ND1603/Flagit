442784db35c2176abd5541ad0a1c67387410539f
Nathnael <dnathnael00@gmail.com>
2026-06-07 22:53:18 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-const generateToken = (id) => {
+const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
 
