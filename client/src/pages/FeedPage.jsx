0484c9090f7fe401121cc6abb1e854b8af6c8392
Nathnael <dnathnael00@gmail.com>
2026-06-28 21:22:31 +0300

    Update FeedPage.jsx

--- client/src/pages/FeedPage.jsx
+++ client/src/pages/FeedPage.jsx
@@ -33,6 +33,10 @@ export default function FeedPage() {
     );
   };
 
+  const handleResolve = (reportId) => {
+    setReports(prev => prev.filter(r => r._id !== reportId));
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-4 page-enter">
 
@@ -109,6 +113,7 @@ export default function FeedPage() {
               key={report._id}
               report={report}
               onUpvote={handleUpvote}
+              onResolve={handleResolve}
             />
           ))}
         </div>
